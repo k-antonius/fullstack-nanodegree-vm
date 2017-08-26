3b0f911efb696a0e1cf6fb366534aa2dff9abb64
Kenneth LaMantia <kennethlamantia@gmail.com>
2017-08-26 07:21:48 -0400

    test: add new pantry to mock db

--- vagrant/catalog/item_catalog/test_db_populator.py
+++ vagrant/catalog/item_catalog/test_db_populator.py
@@ -78,13 +78,14 @@ class MockDB(object):
     def __init__(self):
         '''Creates entities to populate the mock database.
         '''
-        self.mock_users = (User(1, 'A', 'A@aaa.com', 'A_picture', [1,2]),
+        self.mock_users = (User(1, 'A', 'A@aaa.com', 'A_picture', [1,2,4]),
                            User(2, 'B', 'B@bbb.com', 'B_pic', [2,3]),
                            User(3, 'C', 'C@ccc.com', 'C_pic', [3]))
         
         self.pantries = [Pantry(1, 'Pantry_A', 1),
                          Pantry(2, 'Pantry_B', 2),
-                         Pantry(3, 'Pantry_C', 3)]
+                         Pantry(3, 'Pantry_C', 3),
+                         Pantry(4, 'Pantry_D', 1)]
         
         self.categories = [Category(1, 'vegetables', 1),
                            Category(2, 'starches', 1),
